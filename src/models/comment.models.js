b3884ce94bbb5ba310f650ff387eea4f50f76bc0
aayushsapkota01 <sapkotaaayush10@gmail.com>
2024-12-05 13:31:16 -0500

    feat: add MongoDB models with aggregation plugin

---
+++ src/models/comment.models.js
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from "mongoose";
+import aggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const commentSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    video: {
+      type: Schema.Types.ObjectId,
+      ref: "Videos",
+    },
+    owner: {
+      type: Schame.Types.ObjectId,
+      ref: "Users",
+    },
+  },
+  { timestamps: true }
+);
+
+commentSchema.plugin(aggregatePaginate);
+
+export const Comment = mongoose.model("Comment", commentSchema);
---
+++ src/models/users.models.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, //Cloudinary Url
+      required: true,
+    },
+    coverImage: {
+      type: String, //Cloudinary Url
+    },
+    watchHistory: [
+      {
+        type: Schema.types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
